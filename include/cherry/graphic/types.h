089c2972e1b75fd648cbdfff38b131938f934250
Manh <mnh_t@yahoo.com>
2017-02-04 22:52:22 +0700

    add shader, buffer

--- include/cherry/graphic/types.h
+++ include/cherry/graphic/types.h
@@ -4,24 +4,203 @@
 #ifndef __CHERRY_GRAPHIC_TYPES_H__
 #define __CHERRY_GRAPHIC_TYPES_H__
 
+#include <cherry/types.h>
 #include <cherry/graphic/graphic.h>
-#include <cherry/list.h>
 
-struct device_buffer_head {
-        struct list_head head;
+struct device_buffer;
+struct shader;
 
-#if TARGET_RENDERER == RENDERER_OPENGL
-        uint32_t id; /* reference to device buffer */
-#elif TARGET_RENDERER == RENDERER_METAL
-        void *ptr; /* reference to device buffer */
-        uint64_t size;
+/*
+ * OGL
+ * @id          : opengl vbo id
+ *
+ * MTL
+ * @ptr         : bridge pointer to mtl_buffer object
+ * @size        : current size of buffer
+ */
+struct device_buffer {
+#if   GFX == OGL
+        u32     id;
+#elif GFX == MTL
+        void    *ptr;
+        u64     size;
 #endif
 };
 
-struct device_buffer_array_head {
-        struct list_head head;
-#if TARGET_RENDERER == RENDERER_OPENGL
-        uint32_t id;
+/*
+ * @buffers     : array of device_buffer
+ * @pipeline    : current pipeline setting up this group
+ *
+ * OGL
+ * @id          : opengl vao id
+ */
+struct device_buffer_group {
+        struct array    *buffers;
+        struct shader   *pipeline;
+#if GFX == OGL
+        u32             id;
+#endif
+};
+
+/*
+ * uniform data type
+ */
+enum {
+        UNIFORM_F1,
+        UNIFORM_F2,
+        UNIFORM_F3,
+        UNIFORM_F4,
+        UNIFORM_I1,
+        UNIFORM_I2,
+        UNIFORM_I3,
+        UNIFORM_I4,
+        UNIFORM_M2,
+        UNIFORM_M3,
+        UNIFORM_M4
+};
+
+/*
+ * mesh vertex type
+ * @PS  : position
+ * @NR  : normal
+ * @TR  : transformation
+ * @CL  : color
+ */
+enum {
+        PS_NR_TR_CL,
+        PS_NR,
+        PS_TR
+};
+
+/*
+ * shader attribute type
+ */
+enum {
+        ATTRIBUTE_FLOAT,
+        ATTRIBUTE_VEC2,
+        ATTRIBUTE_VEC3,
+        ATTRIBUTE_VEC4,
+        ATTRIBUTE_MAT2,
+        ATTRIBUTE_MAT3,
+        ATTRIBUTE_MAT4
+};
+
+/*
+ * uniform uses this index to keep track to shaders it's linking to
+ * @pipeline    : shader that uniform is linking to
+ * @index       : index of uniform in shader
+ */
+struct shader_uniform_index {
+        struct shader   *pipeline;
+        i16             index;
+};
+
+/*
+ * uniform object
+ * @data        : uniform's data
+ * @ref         : life-time counter
+ * @indice      : array of shaders that uniform is linking to
+ */
+struct shader_uniform {
+        struct bytes    *data;
+        i16             ref;
+        struct array    *indice;
+};
+
+/*
+ * shader uniform tracker
+ * @uniform     : link to uniform object
+ * @pipeline    : shader owning this tracker
+ * @type        : uniform data type
+ * @index       : uniform index in shader
+ *
+ * OGL
+ * @name        : opengl uniform name
+ * @id          : opengl uniform id
+ *
+ * MTL
+ * @offset      : mtl uniform offset in buffer
+ */
+struct shader_uniform_track {
+        struct shader_uniform   *uniform;
+        struct shader           *pipeline;
+#if GFX == OGL
+        struct string           *name;
+        i32                     id;
+#endif
+        u8                      type;
+        i16                     index;
+#if GFX == MTL
+        i16                     offset;
+#endif
+};
+
+/*
+ * shader attribute descriptor
+ * @type        : attribute type
+ *
+ * OGL
+ * name         : attribute name
+ *
+ * MTL
+ * @offset      : attribute offset in buffer
+ */
+struct shader_attribute_descriptor {
+        u8              type;
+#if GFX == OGL
+        struct string   *name;
+#endif
+#if GFX == MTL
+        u16             offset;
+#endif
+};
+
+/*
+ * @attributes  : array of attributes used in buffer
+ * @vertex_size : one vertex size in buffer
+ * @instanced   : indicate buffer is instanced or not
+ */
+struct shader_buffer_descriptor {
+        struct array    *attributes;
+        u16             vertex_size;
+        u8              instanced;
+};
+
+/*
+ * @buffers     : array of buffers used in pipeline
+ */
+struct shader_descriptor {
+        struct array *buffers;
+};
+
+/*
+ * shader object
+ * @tracks      : array of uniform trackers registered
+ * @flags       : array of flag to find active trackers
+ * @pendings    : array of active trackers indice
+ * @update      : flag allows shader to update uniforms datas
+ * @mesh_type   : mesh vertex type allowed in pipeline
+ *
+ * OGL
+ * @id          : opengl shader id
+ *
+ * MTL
+ * @uniforms    : mtl uniforms buffers
+ * @ptr         : bridge pointer to mtl pipeline object
+ */
+struct shader {
+        struct array                    *tracks;
+        struct array                    *flags;
+        struct array                    *pendings;
+        u8                              update[BUFFERS];
+        u8                              mesh_type;
+        struct shader_descriptor        *descriptor;
+#if GFX == OGL
+        u32                             id;
+#endif
+#if GFX == MTL
+        struct device_buffer            *uniforms[BUFFERS];
+        void                            *ptr;
 #endif
 };
 
