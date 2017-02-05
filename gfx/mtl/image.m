8fccecb130d5414d1046bb762ef92a5a0fad2f2b
Manh <mnh_t@yahoo.com>
2017-02-05 17:46:09 +0700

    implement image functions

---
+++ gfx/mtl/image.m
@@ -0,0 +1,97 @@
+#import <cherry/graphic/image.h>
+
+#if OS == MTL && IMAGE_TYPE == IMAGE_OBJC
+
+#import <cherry/memory.h>
+#import <cherry/stdio.h>
+#import <cherry/string.h>
+#import <UIKit/UIKit.h>
+
+static void dataImage(struct image *p, UIImage *uiimage)
+{
+    CGImageRef imageRef = [uiimage CGImage];
+
+    // Create a suitable bitmap context for extracting the bits of the image
+    const NSUInteger width = CGImageGetWidth(imageRef);
+    const NSUInteger height = CGImageGetHeight(imageRef);
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    uint8_t *rawData = (uint8_t *)malloc(height * width * 4);
+    const NSUInteger bytesPerPixel = 4;
+    const NSUInteger bytesPerRow = bytesPerPixel * width;
+    const NSUInteger bitsPerComponent = 8;
+    CGContextRef context = CGBitmapContextCreate(rawData, width, height,
+                                                 bitsPerComponent, bytesPerRow, colorSpace,
+                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGColorSpaceRelease(colorSpace);
+
+    CGContextTranslateCTM(context, 0, height);
+    CGContextScaleCTM(context, 1, -1);
+
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), imageRef);
+    CGContextRelease(context);
+
+    p->ptr = rawData;
+    p->bytes_per_row = bytesPerRow;
+}
+
+/*
+ * allocate new image object from file path
+ */
+struct image *image_alloc(char *file)
+{
+        /* create objc image */
+        struct string *full_path = file_full_path(file);
+        UIImage *uiimage = [UIImage imageWithContentsOfFile:[NSString stringWithUTF8String:full_path->ptr]];
+        string_free(full_path);
+
+        /* create application image */
+        struct image *ret = smalloc(sizeof(struct image));
+        ret->width = uiimage.size.width * uiimage.scale;
+        ret->height = uiimage.size.height * uiimage.scale;
+        dataImage(ret, uiimage);
+        ret->type = MTLPixelFormatRGBA8Unorm;
+        return ret;
+}
+
+/*
+ * deallocate image object
+ */
+void image_free(struct image *p)
+{
+        free(p->ptr);
+        sfree(p);
+}
+
+/*
+ * get image width
+ */
+u16 image_width(struct image *p)
+{
+        return p->width;
+}
+
+/*
+ * get image height
+ */
+u16 image_height(struct image *p)
+{
+        return p->height;
+}
+
+/*
+ * get image pixels
+ */
+void *image_pixels(struct image *p)
+{
+        return p->ptr;
+}
+
+/*
+ * get image type
+ */
+i32 image_type(struct image *p)
+{
+        return p->type;
+}
+
+#endif
---
+++ gfx/sdl2/image.c
@@ -0,0 +1,130 @@
+#include <cherry/graphic/image.h>
+
+#if IMAGE_TYPE == IMAGE_SDL2
+
+#include <cherry/memory.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+/*
+ * allocate new image object from file path
+ */
+struct image *image_alloc(char *file)
+{
+        /* try create surface from file */
+        SDL_Surface *tempimage = IMG_Load(file);
+        if( ! tempimage) return 0;
+
+        /* fix byte order */
+        SDL_Surface *tempsurface;
+        Uint32 rmask, gmask, bmask, amask;
+
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+        rmask = 0xff000000;
+        gmask = 0x00ff0000;
+        bmask = 0x0000ff00;
+        amask = 0x000000ff;
+#else
+        rmask = 0x000000ff;
+        gmask = 0x0000ff00;
+        bmask = 0x00ff0000;
+        amask = 0xff000000;
+#endif
+        tempsurface = SDL_CreateRGBSurface(SDL_SWSURFACE, tempimage->w, tempimage->h, 32, rmask, gmask, bmask, amask);
+        SDL_BlitSurface(tempimage, NULL, tempsurface, NULL);
+        SDL_FreeSurface(tempimage);
+
+        struct image *p = smalloc(sizeof(struct image));
+        p->ptr = tempsurface;
+
+        /* Extracting color components from a 32-bit color value */
+        SDL_PixelFormat *fmt;
+        SDL_Surface *surface = tempsurface;
+        Uint32 temp, pixel;
+        Uint8 red, green, blue, alpha;
+
+        fmt = surface->format;
+        SDL_LockSurface(surface);
+        pixel = *((Uint32*)surface->pixels);
+        SDL_UnlockSurface(surface);
+
+        /* Get Red component */
+        temp = pixel & fmt->Rmask;  /* Isolate red component */
+        temp = temp >> fmt->Rshift; /* Shift it down to 8-bit */
+        temp = temp << fmt->Rloss;  /* Expand to a full 8-bit number */
+        red = (Uint8)temp;
+
+        /* Get Green component */
+        temp = pixel & fmt->Gmask;  /* Isolate green component */
+        temp = temp >> fmt->Gshift; /* Shift it down to 8-bit */
+        temp = temp << fmt->Gloss;  /* Expand to a full 8-bit number */
+        green = (Uint8)temp;
+
+        /* Get Blue component */
+        temp = pixel & fmt->Bmask;  /* Isolate blue component */
+        temp = temp >> fmt->Bshift; /* Shift it down to 8-bit */
+        temp = temp << fmt->Bloss;  /* Expand to a full 8-bit number */
+        blue = (Uint8)temp;
+
+        /* Get Alpha component */
+        temp = pixel & fmt->Amask;  /* Isolate alpha component */
+        temp = temp >> fmt->Ashift; /* Shift it down to 8-bit */
+        temp = temp << fmt->Aloss;  /* Expand to a full 8-bit number */
+        alpha = (Uint8)temp;
+
+        if(alpha == 0)
+        {
+                p->type = GL_RGB;
+        }
+        else
+        {
+                p->type = GL_RGBA;
+        }
+
+        return p;
+}
+
+/*
+ * deallocate image object
+ */
+void image_free(struct image *p)
+{
+        if(p->ptr) {
+                SDL_FreeSurface(p->ptr);
+        }
+        sfree(p);
+}
+
+/*
+ * get image width
+ */
+u16 image_width(struct image *p)
+{
+        return ((SDL_Surface *)p->ptr)->w;
+}
+
+/*
+ * get image height
+ */
+u16 image_height(struct image *p)
+{
+        return ((SDL_Surface *)p->ptr)->h;
+}
+
+/*
+ * get image pixels
+ */
+void *image_pixels(struct image *p)
+{
+        return ((SDL_Surface *)p->ptr)->pixels;
+}
+
+/*
+ * get image type
+ */
+i32 image_type(struct image *p)
+{
+        return p->type;
+}
+
+#endif
